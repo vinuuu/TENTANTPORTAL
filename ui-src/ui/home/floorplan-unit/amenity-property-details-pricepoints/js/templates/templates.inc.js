7b4c673037a354a83b64231a39d8cffb29760eb6
vinuuu <vinodanasuri@gmail.com>
2018-02-09 21:42:59 +0530

    unused folders Removed

--- ui-src/ui/home/floorplan-unit/amenity-property-details-pricepoints/js/templates/templates.inc.js
+++
@@ -1,4 +0,0 @@
-angular.module('ui').run(['$templateCache', function ($templateCache) {
-$templateCache.put("home/floorplan-unit/amenity-property-details-pricepoints/templates/pricepoints-tab.html",
-"Price Points Tab");
-}]);
--- ui-src/ui/home/floorplan-unit/edit-amenity/js/templates/templates.inc.js
+++
@@ -1,4 +0,0 @@
-angular.module('ui').run(['$templateCache', function ($templateCache) {
-$templateCache.put("home/floorplan-unit/edit-amenity/templates/index.html",
-"<div ng-controller=\"FpuEditAmenityCtrl as editAmenity\" class=\"rp-aside-modal rp-aside-modal-right rp-aside-modal-show rp-aside-modal-extend-modal-1\"><div class=\"rp-aside-modal-header rp-aside-modal-header-wrap extend\">Edit Amenity {{editAmenity.amenity}}</div><div class=\"rp-aside-modal-content\"></div></div>");
-}]);
